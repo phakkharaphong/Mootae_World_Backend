6ec4c5c17dd98fa90ea4c77650df1312bb7e45b6
FOLK <folkadpocket99@gmail.com>
2025-12-27 00:13:54 +0700

    change upload method

--- app/features/attachment/router.py
+++ app/features/attachment/router.py
@@ -1,15 +1,7 @@
-from datetime import datetime
 from pathlib import Path
+import shutil
 import uuid
-from fastapi import APIRouter, Depends, File, UploadFile, HTTPException
-import pytz
-from requests import Session
-
-from app.core.database import get_db
-from app.features.attachment.dto import AttachmentCreateDto, AttachmentGetDto
-from app.features.attachment.model import Attachment
-from app.features.attachment.service import create
-from app.utils.response import ResponseModel
+from fastapi import APIRouter, File, Request, UploadFile, HTTPException
 from app.core.config import settings
 
 UPLOAD_DIR = Path(settings.UPLOAD_DIR)
@@ -22,20 +14,8 @@ router = APIRouter(
 )
 
 
-@router.post(
-    "/",
-    response_model=ResponseModel,
-    tags=["attachment"],
-    summary="Create Attachment",
-)
-async def create_attachment(
-    attachment: AttachmentCreateDto, db: Session = Depends(get_db)
-):
-    return create(db, attachment)
-
-
 @router.post("/upload", status_code=201)
-async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
+async def upload_file(request: Request, file: UploadFile = File(...)):
     if file.content_type not in {"image/png", "image/jpeg"}:
         raise HTTPException(400, "Invalid file type")
 
@@ -44,36 +24,11 @@ async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db
     path = UPLOAD_DIR / filename
 
     with path.open("wb") as f:
-        while chunk := await file.read(1024 * 1024):
-            f.write(chunk)
+        shutil.copyfileobj(file.file, f)
 
-    new_attachment = Attachment(
-        id=str(uuid.uuid4()),
-        fileName=file.filename,
-        content_type=file.content_type,
-        fileLocation=str(path),
-        fileSize=path.stat().st_size,
-        mime=ext,
-        created_at=datetime.now(pytz.timezone("Asia/Bangkok")),
-        created_by="System",
-    )
-
-    db.add(new_attachment)
-    db.commit()
-    db.refresh(new_attachment)
+    file_url = f"{request.base_url}uploads/files/{filename}"
 
     return {
-        "original_name": file.filename,
-        "stored_name": filename,
-        "content_type": file.content_type,
-        "path": str(path),
+        "filename": filename,
+        "url": file_url,
     }
-
-
-@router.get("/{id}", tags=["attachment"], summary="Get detail file")
-async def get_attachment(id: str, db: Session = Depends(get_db)):
-    attachment = db.query(Attachment).filter(Attachment.id == id).first()
-    if not attachment:
-        raise HTTPException(404, "Attachment not found")
-    attachment_dto = AttachmentGetDto.model_validate(vars(attachment))
-    return ResponseModel(status=200, message="Success", data=attachment_dto)
